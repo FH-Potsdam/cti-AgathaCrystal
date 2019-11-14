42f2e46b5065d362b8b1d555d1aa0d69f7359b7c
AgathaCrystal <56319144+AgathaCrystal@users.noreply.github.com>
2019-11-14 23:52:25 +0100

    Hausaufgabe zum 14.11.

--- packages/area/index.js
+++ packages/area/index.js
@@ -4,26 +4,42 @@
  * @todo draw area from center not upper left corner
  * @todo do something on click for the area
  */
+
+//########################
+let wasRotated = false;
+let transX = 0;
+let transY = 0;
+//########################
+
+let lastOver;
+
+
+
 let x = 0;
 let canvas = undefined;
 const step = 25;
 const areas = [];
 function setup() {
-  canvas = createCanvas(100, 100);
+  canvas = createCanvas(500, 500);
   canvas.parent("sketch");
   for (let x = 0; x < width; x += step) {
     for (let y = 0; y < height; y += step) {
       areas.push(new Area(x, y, step, step));
     }
   }
-  noStroke();
+  //noStroke();
 }
 
 function draw() {
+  console.log("redraw all areas")
   for (const item of areas) {
     item.update(mouseX, mouseY);
     item.display();
   }
+
+  // if (lastOver) {
+  //   lastOver.display();
+  // }
 }
 
 function Area(x, y, w, h) {
@@ -38,7 +54,11 @@ function Area(x, y, w, h) {
   this.h = h;
   this.isOver = false;
 
-  this.update = function(mX, mY) {
+  this.areaColor = "#FFFFFF";
+  this.rotationAngle = 0;
+
+
+  this.update = function (mX, mY) {
     if (
       mX > this.x &&
       mX < this.x + this.w &&
@@ -46,26 +66,82 @@ function Area(x, y, w, h) {
       mY < this.y + this.h
     ) {
       this.isOver = true;
+      lastOver = this;
     } else {
       this.isOver = false;
     }
   };
 
-  this.display = function() {
+
+  // this.display = function () {
+  //   if (this.isOver === true) {
+  //     fill("#ff6347");
+
+      
+  //   } else {
+  //     fill(this.areaColor);
+
+  //   }
+  //   if (this.isOver) {
+     
+  //     rect(this.x, this.y, this.w * 2, this.h * 2);
+  //   } else {
+  //     rect(this.x, this.y, this.w, this.h);
+  //   }
+  // };
+
+
+  this.display = function () {
+
+
     if (this.isOver === true) {
+      transX = (this.w / 2) + x;
+      transY = (this.h / 2) + y;
+  
       fill("#ff6347");
+  
+      translate(transX, transY); // zum ursprung projezieren (0,0)
+      rotate(PI / 3.6); // rotieren
+      translate(-transX,-transY); // zurück zur Herkunftsposition projezieren
+      wasRotated = true;
+  
     } else {
+      if (wasRotated) {
+        translate(transX, transY); //  zum ursprung projezieren (0,0)
+        rotate(-(PI / 3.6)); // "zurück" rotieren
+        translate(-(transX),-(transY)); // zurück zur Herkunftsposition projezieren
+        wasRotated = false;
+      }
       fill("#00ff00");
     }
+    stroke("#ffffff");
     rect(this.x, this.y, this.w, this.h);
   };
+
+
+
 }
 
-function keyPressed() {
-  if (key === "s" || key === "S") {
-    if (canvas === undefined) {
-      throw new Error("Could not find your canvas");
+
+function mouseClicked(event) {
+  for (const area of areas) {
+    if (mouseX > area.x && mouseX < area.x + area.w &&
+      mouseY > area.y && mouseY < area.y + area.h) {
+        area.areaColor  = "#000000";
+        
     }
-    saveCanvas(canvas, "sketch", "png");
   }
 }
+
+
+
+
+
+// function keyPressed() {
+//   if (key === "s" || key === "S") {
+//     if (canvas === undefined) {
+//       throw new Error("Could not find your canvas");
+//     }
+//     saveCanvas(canvas, "sketch", "png");
+//   }
+// }
